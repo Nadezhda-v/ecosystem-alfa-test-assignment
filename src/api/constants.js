16dabcf0d677431a5333599453828d77c04761c5
Nadezhda Vorotova <work-nadezhda-v@yandex.ru>
2024-01-18 10:09:06 +0400

    fix: update REDIRECT_URI

--- src/api/constants.js
+++ src/api/constants.js
@@ -3,7 +3,7 @@ const URL_API = 'https://api.unsplash.com/';
 const ACCESS_KEY = 'NendjEF5uSMZgCKXpYLhjqIHqyi6NJ6DtOO1fSYa_Vo';
 const CLIENT_SECRET = 'quxiaczdSIxQwDc6QyiAJdPFYbGqkO2LQKr9QsTAuNc';
 const RESPONSE_TYPE = 'code';
-const REDIRECT_URI = 'http://localhost:3000/';
+const REDIRECT_URI = 'http://ecosystem-alfa-test-assignment.vercel.app';
 const SCOPE_STRING = 'public read_user read_photos write_likes';
 const GRANT_TYPE = 'authorization_code';
 
